d09e106f159a2692a9d36d5a1edfd267e021342c
Arjan Schrijver <a_gadgetbridge@anymore.nl>
2021-06-02 21:24:16 +0200

    Add raw format output and command usage help to image_compress.py

--- tools/image_compress.py
+++ tools/image_compress.py
@@ -29,7 +29,7 @@ class Image_compresser:
 
         return output
 
-    def compress(self, input_file, width, height, output_file):
+    def compress_rle(self, input_file, width, height, output_file):
         image = Image.open(input_file)
         if image.getbands() != tuple(['R', 'G', 'B', 'A']):
             raise Exception('image bands have to be RGBA')
@@ -53,14 +53,53 @@ class Image_compresser:
         with open(output_file, 'wb') as output_file:
             output_file.write(output)
 
+    def compress_raw(self, input_file, width, height, output_file):
+        image = Image.open(input_file)
+        pixels = bytearray(width * height)
+        jumpX = image.width / width
+        jumpY = image.height / height
+
+        for y in range(0, height):
+            for x in range(0, width):
+                pixel = image.getpixel((x * jumpX, y * jumpY))
+                grey = (pixel[0] + pixel[1] + pixel[2]) / 3
+                pixels[len(pixels) - 1 - (y * width + x)] = int(grey)
+
+        output = bytearray(int(len(pixels) / 4))
+        for i in range(0, len(pixels)):
+            resultPixelIndex = int(i / 4)
+            shiftIndex = 6 - i % 4 * 2
+            output[resultPixelIndex] |= ((int(pixels[i]) & 0xFF) >> 6) << shiftIndex
+
+        with open(output_file, 'wb') as output_file:
+            output_file.write(output)
+
+
+def usage():
+    print('Usage: ' + sys.argv[0] + ' [options]')
+    print('Available options are:')
+    print('  -i,  --input=FILE      input file (RGBA PNG)')
+    print('  -o,  --output=FILE     output file')
+    print('  -w,  --width=PIXELS    width of the output image in pixels')
+    print('  -h,  --height=PIXELS   height of the output image in pixels')
+    print('  -f,  --format=FORMAT   format of the output image (rle or raw)')
 
 def main():
     input_file = None
     output_file = None
     width = -1
     height = -1
+    output_format = None
 
-    options, remainder = getopt.getopt(sys.argv[1:], 'i:w:h:o:', ['input=', 'width=', 'height=', 'output='])
+    if len(sys.argv) < 11:
+        usage()
+        sys.exit(2)
+
+    try:
+        options, remainder = getopt.getopt(sys.argv[1:], 'i:w:h:o:f:', ['input=', 'width=', 'height=', 'output=', 'format='])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
 
     for key, value in options:
         if key in ['-i', '--input']:
@@ -71,14 +110,27 @@ def main():
             width = int(value)
         elif key in ['-h', '--height']:
             height = int(value)
+        elif key in ['-f', '--format']:
+            output_format = value
+            if output_format not in ['rle', 'raw']:
+                usage()
+                sys.exit(2)
 
     compresser = Image_compresser()
-    compresser.compress(
-        input_file,
-        width,
-        height,
-        output_file
-    )
+    if output_format == 'rle':
+        compresser.compress_rle(
+            input_file,
+            width,
+            height,
+            output_file
+        )
+    elif output_format == 'raw':
+        compresser.compress_raw(
+            input_file,
+            width,
+            height,
+            output_file
+        )
 
 
 if __name__ == '__main__':
